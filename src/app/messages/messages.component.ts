02705c08508631fc8452a81994d290d8f8313902
zatkiller <gheedylan@yahoo.com.sg>
2021-06-16 01:52:30 +0800

    Add message service to hero service

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.css']
+  styleUrls: ['./messages.component.css'],
 })
 export class MessagesComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
