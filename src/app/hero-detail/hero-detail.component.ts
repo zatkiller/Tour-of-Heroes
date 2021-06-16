31db6777d32c486b0420d3cefe39008a8d2fd943
zatkiller <gheedylan@yahoo.com.sg>
2021-06-17 03:30:46 +0800

    Go back button using location and styling

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -27,4 +27,8 @@ export class HeroDetailComponent implements OnInit {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.heroService.getHero(id).subscribe((hero) => (this.hero = hero));
   }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
