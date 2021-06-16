13815a0a667c609a9e01fb3f4c20d1ff753a25cf
zatkiller <gheedylan@yahoo.com.sg>
2021-06-17 03:54:12 +0800

    Add get hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -27,10 +27,13 @@ export class HeroService {
     );
   }
 
+  /** GET hero by id. Will 404 if id not found */
   getHero(id: number): Observable<Hero> {
-    const hero = HEROES.find((h) => h.id === id)!;
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(hero);
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap((_) => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
 
   private log(message: string) {
